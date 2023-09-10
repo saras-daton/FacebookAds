7110e56e07cd4dab8fa6330916aead46454fbe98
smriti-saras <141820150+smriti-saras@users.noreply.github.com>
2023-09-10 23:54:47 +0530

    facebook ad design update

--- models/FacebookAds/FacebookAdinsights.sql
+++ models/FacebookAds/FacebookAdinsights.sql
@@ -76,7 +76,7 @@ with fbadinsights as (
         account_name,
         --actions,
         --action_values,
-        coalesce(ad_id,'') as ad_id,
+        coalesce(ad_id,'NA') as ad_id,
         ad_name,
         adset_id,
         adset_name,
@@ -96,7 +96,7 @@ with fbadinsights as (
         cpm,
         cpp,
         ctr,
-        CAST(date_start as DATE) date_start,
+        coalesce(CAST(date_start as DATE),'NA') date_start,
         CAST(date_stop as DATE) date_stop,
         estimated_ad_recall_rate,
         estimated_ad_recallers,
@@ -111,8 +111,33 @@ with fbadinsights as (
         CAST(unique_clicks as INT64) AS unique_clicks,
         unique_ctr,
         unique_inline_link_click_ctr,
+
         CAST(unique_inline_link_clicks as INT64) as unique_inline_link_clicks,
-        --website_ctr,
+        {{extract_nested_value("website_ctr","_daton_pre_1d_click","string")}} as website_ctr_1d_click,
+        {{extract_nested_value("website_ctr","_daton_pre_1d_view","string")}} as website_ctr_1d_view,
+        {{extract_nested_value("website_ctr","_daton_pre_28d_click","string")}} as website_ctr_28d_click,
+        {{extract_nested_value("website_ctr","_daton_pre_28d_view","string")}} as website_ctr_28d_view,
+        {{extract_nested_value("website_ctr","_daton_pre_7d_click","string")}} as website_ctr_7d_click,
+        {{extract_nested_value("website_ctr","_daton_pre_7d_view","string")}} as website_ctr_7d_view,
+        {{extract_nested_value("website_ctr","action_canvas_component_id","string")}} as website_ctr_action_canvas_component_id,
+        {{extract_nested_value("website_ctr","action_canvas_component_name","string")}} as website_ctr_action_canvas_component_name,
+        {{extract_nested_value("website_ctr","action_carousel_card_id","string")}} as website_ctr_action_carousel_card_id,
+        {{extract_nested_value("website_ctr","action_carousel_card_name","string")}} as website_ctr_action_carousel_card_name,
+        {{extract_nested_value("website_ctr","action_destination","string")}} as website_ctr_action_destination,
+        {{extract_nested_value("website_ctr","action_device","string")}} as website_ctr_action_device,
+        {{extract_nested_value("website_ctr","action_event_channel","string")}} as website_ctr_action_event_channel,
+        {{extract_nested_value("website_ctr","action_link_click_destination","string")}} as website_ctr_action_link_click_destination,
+        {{extract_nested_value("website_ctr","action_location_code","string")}} as website_ctr_action_location_code,
+        {{extract_nested_value("website_ctr","action_reaction","string")}} as website_ctr_action_reaction,
+        {{extract_nested_value("website_ctr","action_target_id","string")}} as website_ctr_action_target_id,
+        {{extract_nested_value("website_ctr","action_type","string")}} as website_ctr_action_type,
+        {{extract_nested_value("website_ctr","action_video_asset_id","string")}} as website_ctr_action_video_asset_id,
+        {{extract_nested_value("website_ctr","action_video_sound","string")}} as website_ctr_action_video_sound,
+        {{extract_nested_value("website_ctr","action_video_type","string")}} as website_ctr_action_video_type,
+        {{extract_nested_value("website_ctr","inline","string")}} as website_ctr_inline,
+        {{extract_nested_value("website_ctr","value","string")}} as website_ctr_value,
+        {{extract_nested_value("website_ctr","id","string")}} as website_ctr_id,
+       
         {{daton_user_id()}} as _daton_user_id,
         {{daton_batch_runtime()}} as _daton_batch_runtime,
         {{daton_batch_id()}} as _daton_batch_id,
@@ -120,6 +145,7 @@ with fbadinsights as (
         '{{env_var("DBT_CLOUD_RUN_ID", "manual")}}' as _run_id,
         ROW_NUMBER() OVER (PARTITION BY ad_id,date_start order by {{daton_batch_runtime()}} desc) row_num
             from {{i}}
+                {{unnesting("website_ctr")}}
                 {% if is_incremental() %}
                 {# /* -- this filter will only be applied on an incremental run */ #}
                 WHERE {{daton_batch_runtime()}}  >= {{max_loaded}}
